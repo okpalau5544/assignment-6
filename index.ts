2a112cddbe1bd60bbfed635c44c11f922ccc023e
vivian55636 <okpalau@mcmaster.ca>
2025-07-07 02:51:15 -0400

    initial commit

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
 import server from './server'
+import express from 'express';
+const app = express();
 
-await server(3000)
+app.get('/', (req, res) => {
+  res.send('Hello from the API!');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
+});
+
+
